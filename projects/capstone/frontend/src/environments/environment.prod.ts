ab5c9a2d5354c3d9838fe4c6f2fd0e10f4297033
nlucasti <53306298+nlucasti@users.noreply.github.com>
2020-06-09 10:41:06 -0400

    Add files via upload

---
+++ projects/capstone/frontend/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  apiServerUrl: 'https://fsnd-casting-app.herokuapp.com/', // the running FLASK api server url
+  auth0: {
+    url: 'nl-fsnd', // the auth0 domain prefix
+    audience: 'casting', // the audience set for the auth0 app
+    clientId: 'iXBHbMKRXOs8Krk3TfioJQ8WOckcDH0e', // the client id generated for the auth0 app
+    callbackURL: 'https://fsnd-casting-frontend.herokuapp.com', // the base url of the running ionic application.
+  }
+};
+//
