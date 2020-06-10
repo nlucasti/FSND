ce645fb01a0eb91efac11ed76bd4774b20f977fc
nlucasti <53306298+nlucasti@users.noreply.github.com>
2020-06-10 19:57:03 -0400

    Updated Shell Script

--- projects/capstone/backend/setup.sh
+++ projects/capstone/backend/setup.sh
@@ -1,1 +1,4 @@
-
+export AUTH0_DOMAIN= 'nl-fsnd.auth0.com'
+export API_AUDIENCE= 'casting'
+export ALGORITHMS= ['RS256']
+export DATABASE_URL= 'postgres://liurhkzcwduchj:fb5aa5513f666b6c41905d224aec9b7dabc4a8375f48e815e6cbd74a800dfd3c@ec2-34-230-231-71.compute-1.amazonaws.com:5432/d1ogjolbhbbaj3'
